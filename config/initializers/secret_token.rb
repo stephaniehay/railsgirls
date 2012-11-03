dd3037657120e1d443894067a45d3454c28fa366
Stephanie Hay <steph@stephaniehay.com>
2012-11-03 13:22:24 -0400

    Pushing all the shizzle to MrGitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'c795e43009999a6febacc998071bbbee17741984ebc8adb3b158555e9c53ecb94684884a1eb9ddbd14936a09c62646572843d1bd7b398bfc4ed6d9146d04bba3'
